alvis/.agent

refactor(web): migrate imagine skill tools to TypeScript
Move the imagine skill's batch image generation from Python scripts to Bun TypeScript with sibling Vitest specs, preserving the retired oracle's observable contract: per-job starting/completed progress, provider-call timing and stderr logging, failure and exit-code behavior, and doc link-integrity checks.

plugins/web/skills/audit/scripts/serve.spec.ts
@@ -1,0 +1,109 @@
+import { once } from "node:events";
+import { spawn, type ChildProcessWithoutNullStreams } from "node:child_process";
+import { fileURLToPath } from "node:url";
+
+const servePath = fileURLToPath(new URL("./serve.ts", import.meta.url));
+
+interface RunningServer {
+  readonly child: ChildProcessWithoutNullStreams;
+  readonly port: number;
+}
+
+async function startServer(): Promise<RunningServer> {
+  const child = spawn("bun", [servePath], { stdio: "pipe" });
+  let output = "";
+
+  return await new Promise<RunningServer>((resolve, reject) => {
+    const timer = setTimeout(() => {
+      child.kill("SIGTERM");
+      reject(new Error("serve.ts did not publish a port handshake"));
+    }, 5_000);
+
+    child.stdout.on("data", (chunk: Buffer) => {
+      output += chunk.toString();
+      const match = output.match(/SERVING_PORT:(\d+)/);
+      if (!match) return;
+      clearTimeout(timer);
+      resolve({ child, port: Number(match[1]) });
+    });
+    child.once("error", (error) => {
+      clearTimeout(timer);
+      reject(error);
+    });
+    child.once("exit", (code, signal) => {
+      if (code !== null || signal !== null) return;
+      clearTimeout(timer);
+      reject(new Error("serve.ts exited before publishing a port"));
+    });
+  });
+}
+
+async function stopServer(server: RunningServer): Promise<void> {
+  if (server.child.exitCode !== null || server.child.signalCode !== null)
+    return;
+  server.child.kill("SIGTERM");
+  await once(server.child, "exit");
+}
+
+describe("audit script file server", () => {
+  let server: RunningServer | undefined;
+
+  afterEach(async () => {
+    if (server) await stopServer(server);
+    server = undefined;
+  });
+
+  it("publishes an ephemeral localhost port and serves the script root", async () => {
+    server = await startServer();
+
+    expect(server.port).toBeGreaterThanOrEqual(49_152);
+    expect(server.port).toBeLessThanOrEqual(65_535);
+
+    const response = await fetch(`http://127.0.0.1:${server.port}/`);
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toContain("text/html");
+  });
+
+  it("serves opaque script bytes for GET and preserves metadata for HEAD", async () => {
+    server = await startServer();
+    const url = `http://127.0.0.1:${server.port}/serve.ts`;
+
+    const get = await fetch(url);
+    const head = await fetch(url, { method: "HEAD" });
+
+    expect(get.status).toBe(200);
+    expect(Number(get.headers.get("content-length"))).toBeGreaterThan(0);
+    expect(get.headers.get("content-type")).toContain("javascript");
+    expect(head.status).toBe(200);
+    expect(head.headers.get("content-length")).toBe(
+      get.headers.get("content-length"),
+    );
+    expect((await head.arrayBuffer()).byteLength).toBe(0);
+  });
+
+  it("rejects unsupported methods, missing files, and traversal paths", async () => {
+    server = await startServer();
+    const root = `http://127.0.0.1:${server.port}`;
+
+    const unsupported = await fetch(`${root}/serve.ts`, { method: "POST" });
+    const missing = await fetch(`${root}/missing.ts`);
+    const traversal = await fetch(`${root}/..%2f..%2fAGENTS.md`);
+
+    expect(unsupported.status).toBe(501);
+    expect(missing.status).toBe(404);
+    expect(traversal.status).toBe(404);
+  });
+
+  it("keeps separate server processes isolated and terminates on signal", async () => {
+    const first = await startServer();
+    const second = await startServer();
+    server = second;
+
+    expect(second.port).not.toBe(first.port);
+    await stopServer(first);
+    await expect(fetch(`http://127.0.0.1:${first.port}/`)).rejects.toThrow();
+
+    const response = await fetch(`http://127.0.0.1:${second.port}/`);
+    expect(response.status).toBe(200);
+  });
+});

plugins/web/skills/imagine/scripts/prompt.spec.ts
@@ -1,0 +1,86 @@
+import { mkdtemp, rm, writeFile } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+
+import { ImagineError } from "./providers/base";
+import {
+  PROMPT_FIELDS,
+  _augment_prompt,
+  _augment_prompt_fields,
+  _fields_from_args,
+  _read_prompt,
+} from "./prompt";
+
+describe("Imagine prompt contracts", () => {
+  const roots: string[] = [];
+  afterEach(async () => {
+    for (const root of roots.splice(0))
+      await rm(root, { recursive: true, force: true });
+  });
+
+  it("enforces prompt source exclusivity and trims prompt files", async () => {
+    const root = await mkdtemp(join(tmpdir(), "imagine-prompt-"));
+    roots.push(root);
+    const file = join(root, "prompt.txt");
+    await writeFile(file, "  opaque prompt source  \n");
+
+    expect(_read_prompt("  direct prompt  ", null)).toBe("direct prompt");
+    expect(_read_prompt(null, file)).toBe("opaque prompt source");
+    expect(() => _read_prompt("direct", file)).toThrow(ImagineError);
+    expect(() => _read_prompt(null, join(root, "missing.txt"))).toThrow(
+      "Prompt file not found",
+    );
+    expect(() => _read_prompt(null, null)).toThrow("Missing prompt");
+  });
+
+  it("extracts every declared prompt field and augments in stable order", () => {
+    const args = {
+      augment: true,
+      use_case: "marketing",
+      scene: "studio",
+      subject: "product",
+      style: "editorial",
+      composition: "centered",
+      lighting: "soft",
+      palette: "blue",
+      materials: "glass",
+      text: "opaque label",
+      constraints: "clean",
+      negative: "noise",
+    };
+    expect(Object.keys(_fields_from_args(args))).toEqual([...PROMPT_FIELDS]);
+    expect(_augment_prompt(args, "opaque request")).toBe(
+      [
+        "Use case: marketing",
+        "Primary request: opaque request",
+        "Scene/background: studio",
+        "Subject: product",
+        "Style/medium: editorial",
+        "Composition/framing: centered",
+        "Lighting/mood: soft",
+        "Color palette: blue",
+        "Materials/textures: glass",
+        'Text (verbatim): "opaque label"',
+        "Constraints: clean",
+        "Avoid: noise",
+      ].join("\n"),
+    );
+  });
+
+  it("keeps references under no-augment and omits empty fields", () => {
+    const fields = { scene: "", text: "", style: "editorial" };
+    expect(
+      _augment_prompt_fields(false, "opaque request", fields, {
+        references: ["opaque-reference.bin"],
+      }),
+    ).toBe(
+      [
+        "Style reference: use the provided reference image(s) to guide the visual style, color palette, and texture of the output.",
+        "opaque request",
+      ].join("\n"),
+    );
+    expect(_augment_prompt_fields(true, "request", fields)).toBe(
+      ["Primary request: request", "Style/medium: editorial"].join("\n"),
+    );
+  });
+});

alvis/.agents

plugins/web/skills/audit/scripts/serve.spec.ts
@@ -1,0 +1,113 @@
+import { spawn } from "node:child_process";
+import { once } from "node:events";
+import { join } from "node:path";
+
+import { afterEach, describe, expect, it } from "vitest";
+
+import type { ChildProcessWithoutNullStreams } from "node:child_process";
+
+const servePath = join(import.meta.dirname, "serve.ts");
+
+interface RunningServer {
+  readonly child: ChildProcessWithoutNullStreams;
+  readonly port: number;
+}
+
+async function startServer(): Promise<RunningServer> {
+  const child = spawn("bun", [servePath], { stdio: "pipe" });
+  let output = "";
+
+  return await new Promise<RunningServer>((resolve, reject) => {
+    const timer = setTimeout(() => {
+      child.kill("SIGTERM");
+      reject(new Error("serve.ts did not publish a port handshake"));
+    }, 5_000);
+
+    child.stdout.on("data", (chunk: Buffer) => {
+      output += chunk.toString();
+      const match = output.match(/SERVING_PORT:(\d+)/);
+      if (!match) return;
+      clearTimeout(timer);
+      resolve({ child, port: Number(match[1]) });
+    });
+    child.once("error", (error) => {
+      clearTimeout(timer);
+      reject(error);
+    });
+    child.once("exit", (code, signal) => {
+      if (code !== null || signal !== null) return;
+      clearTimeout(timer);
+      reject(new Error("serve.ts exited before publishing a port"));
+    });
+  });
+}
+
+async function stopServer(server: RunningServer): Promise<void> {
+  if (server.child.exitCode !== null || server.child.signalCode !== null)
+    return;
+  server.child.kill("SIGTERM");
+  await once(server.child, "exit");
+}
+
+describe("audit script static file serving", () => {
+  let server: RunningServer | undefined;
+
+  afterEach(async () => {
+    if (server) await stopServer(server);
+    server = undefined;
+  });
+
+  it("publishes an ephemeral localhost port and serves the script root", async () => {
+    server = await startServer();
+
+    expect(server.port).toBeGreaterThanOrEqual(49_152);
+    expect(server.port).toBeLessThanOrEqual(65_535);
+
+    const response = await fetch(`http://127.0.0.1:${server.port}/`);
+    expect(response.status).toBe(200);
+    expect(response.headers.get("content-type")).toContain("text/html");
+  });
+
+  it("serves opaque script bytes for GET and preserves metadata for HEAD", async () => {
+    server = await startServer();
+    const url = `http://127.0.0.1:${server.port}/serve.ts`;
+
+    const get = await fetch(url);
+    const head = await fetch(url, { method: "HEAD" });
+
+    expect(get.status).toBe(200);
+    expect(Number(get.headers.get("content-length"))).toBeGreaterThan(0);
+    expect(get.headers.get("content-type")).toContain("javascript");
+    expect(head.status).toBe(200);
+    expect(head.headers.get("content-length")).toBe(
+      get.headers.get("content-length"),
+    );
+    expect((await head.arrayBuffer()).byteLength).toBe(0);
+  });
+
+  it("rejects unsupported methods, missing files, and traversal paths", async () => {
+    server = await startServer();
+    const root = `http://127.0.0.1:${server.port}`;
+
+    const unsupported = await fetch(`${root}/serve.ts`, { method: "POST" });
+    const missing = await fetch(`${root}/missing.ts`);
+    const traversal = await fetch(`${root}/..%2f..%2fAGENTS.md`);
+
+    expect(unsupported.status).toBe(501);
+    expect(missing.status).toBe(404);
+    expect(traversal.status).toBe(404);
+  });
+
+  it("keeps separate server processes isolated and terminates on signal", async () => {
+    const first = await startServer();
+    const second = await startServer();
+    server = second;
+
+    expect(second.port).not.toBe(first.port);
+    await stopServer(first);
+    await expect(fetch(`http://127.0.0.1:${first.port}/`)).rejects.toThrow();
+
+    const response = await fetch(`http://127.0.0.1:${second.port}/`);
+    expect(response.status).toBe(200);
+  });
+});

plugins/web/skills/imagine/scripts/prompt.spec.ts
@@ -1,0 +1,88 @@
+import { mkdtemp, rm, writeFile } from "node:fs/promises";
+import { tmpdir } from "node:os";
+import { join } from "node:path";
+
+import { afterEach, describe, expect, it } from "vitest";
+
+import { ImagineError } from "./providers/base";
+import {
+  PROMPT_FIELDS,
+  _augment_prompt,
+  _augment_prompt_fields,
+  _fields_from_args,
+  _read_prompt,
+} from "./prompt";
+
+describe("imagine prompt assembly", () => {
+  const roots: string[] = [];
+  afterEach(async () => {
+    for (const root of roots.splice(0))
+      await rm(root, { recursive: true, force: true });
+  });
+
+  it("enforces prompt source exclusivity and trims prompt files", async () => {
+    const root = await mkdtemp(join(tmpdir(), "imagine-prompt-"));
+    roots.push(root);
+    const file = join(root, "prompt.txt");
+    await writeFile(file, "  opaque prompt source  \n");
+
+    expect(_read_prompt("  direct prompt  ", null)).toBe("direct prompt");
+    expect(_read_prompt(null, file)).toBe("opaque prompt source");
+    expect(() => _read_prompt("direct", file)).toThrow(ImagineError);
+    expect(() => _read_prompt(null, join(root, "missing.txt"))).toThrow(
+      "Prompt file not found",
+    );
+    expect(() => _read_prompt(null, null)).toThrow("Missing prompt");
+  });
+
+  it("extracts every declared prompt field and augments in stable order", () => {
+    const args = {
+      augment: true,
+      use_case: "marketing",
+      scene: "studio",
+      subject: "product",
+      style: "editorial",
+      composition: "centered",
+      lighting: "soft",
+      palette: "blue",
+      materials: "glass",
+      text: "opaque label",
+      constraints: "clean",
+      negative: "noise",
+    };
+    expect(Object.keys(_fields_from_args(args))).toEqual([...PROMPT_FIELDS]);
+    expect(_augment_prompt(args, "opaque request")).toBe(
+      [
+        "Use case: marketing",
+        "Primary request: opaque request",
+        "Scene/background: studio",
+        "Subject: product",
+        "Style/medium: editorial",
+        "Composition/framing: centered",
+        "Lighting/mood: soft",
+        "Color palette: blue",
+        "Materials/textures: glass",
+        'Text (verbatim): "opaque label"',
+        "Constraints: clean",
+        "Avoid: noise",
+      ].join("\n"),
+    );
+  });
+
+  it("keeps references under no-augment and omits empty fields", () => {
+    const fields = { scene: "", text: "", style: "editorial" };
+    expect(
+      _augment_prompt_fields(false, "opaque request", fields, {
+        references: ["opaque-reference.bin"],
+      }),
+    ).toBe(
+      [
+        "Style reference: use the provided reference image(s) to guide the visual style, color palette, and texture of the output.",
+        "opaque request",
+      ].join("\n"),
+    );
+    expect(_augment_prompt_fields(true, "request", fields)).toBe(
+      ["Primary request: request", "Style/medium: editorial"].join("\n"),
+    );
+  });
+});